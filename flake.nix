a689c61cebe9a1417991d10d6c833cd9ba088b39
valdo766hi <rivaldosilalahi13@gmail.com>
2026-03-22 08:20:27 +0700

    feat: add flake.nix with devenv setup

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    argonaut = {
+      url = "github:darksworm/argonaut";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, argonaut, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  # https://devenv.sh/reference/options/
+                  packages = [
+                    pkgs.hello
+                    pkgs.kubectl
+                    pkgs.argocd
+                    pkgs.kubectl-cnpg
+                    argonaut.packages.${system}.default
+                  ];
+
+                  enterShell = ''
+                    hello
+                  '';
+
+                  processes.hello.exec = "hello";
+                }
+              ];
+            };
+          });
+    };
+}
